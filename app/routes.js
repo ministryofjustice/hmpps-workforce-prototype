c4889e66c119c4d42e53c0bd0610beb19f401634
liammcmurray <liammcmurray@gmail.com>
2021-03-17 09:49:08 +0000

    turned off error screen

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,25 @@ const { get } = require('browser-sync')
 const express = require('express')
 const router = express.Router()
 const routesAllocations0 = require('./routesAllocations0')
+const v2 = require('./routes/v2.js')
 
 // Add your routes here - above the module.exports line
 
 router.use('/allocations/0', routesAllocations0)
 
 // Call in routes file from routes folder to keep routes.js cleaner
-router.use('/v3', require('./routes/v3.js'))
+router.use('/v2', v2)
+
+router.post('/allocate-handler', function (req, res) {
+  var allocated = req.session.data['selected']
+  if (allocated == "yes") {
+    res.redirect('/v2/allocation-complete')
+  }
+  else {
+    res.redirect('/v2/allocation-error')
+  }
+
+})
+
 
 module.exports = router
