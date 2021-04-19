811c9cf1ecc05d5d28d22bb5ea01f0a9d2c59cce
jonhindle <jon.hindle@digital.justice.gov.uk>
2021-04-19 15:58:15 +0100

    jh-WFP-76-reductions
    jh-WFP-76-reductions

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,18 @@
 const { get } = require('browser-sync')
 const express = require('express')
 const router = express.Router()
-const routesAllocations0 = require('./routesAllocations0')
+const routesAllocations0 = require('./routes/allocationsV0.js')
+const v1 = require('./routes/v1.js')
 const v2 = require('./routes/v2.js')
+const v3 = require('./routes/v3.js')
 
 // Add your routes here - above the module.exports line
-
 router.use('/allocations/0', routesAllocations0)
 
 // Call in routes file from routes folder to keep routes.js cleaner
+router.use('/v1', v1)
 router.use('/v2', v2)
+router.use('/v3', v3)
 
 router.post('/allocate-handler', function (req, res) {
   var allocated = req.session.data['selected']
