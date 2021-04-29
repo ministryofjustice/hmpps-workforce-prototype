f6a792e65a9bca747470588df601678b5bbb16bd
liammcmurray <liammcmurray@gmail.com>
2021-04-29 18:19:58 +0100

    added NQO flag and plumbed in allocation

--- app/routes/v6.js
+++ app/routes/v6.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const { generateTeam } = require('../data/generateData')
 const { getTeamTotals, getPractitioner, getCasesDueToEnd, getSentencesDueToEnd, getParoleReportsDue } = require('../data/filterHelpers')
 
-const team = generateTeam(9, 'N57BRP', 'N57A')
+const team = generateTeam(12, 'N57BRP', 'N57A')
 
 router.use('*', (req, { locals }, next) => {
-  Object.assign(locals, { team, teamTotals: getTeamTotals(team), url: req.originalUrl, versionUrl: '/v3'  })
+  Object.assign(locals, { team, teamTotals: getTeamTotals(team), url: req.originalUrl, versionUrl: '/v6'  })
   next()
 })
 
@@ -18,18 +18,18 @@ router.get('/officer-view/:id',({ params: { id } }, res) => {
     sentencesDueToEnd: getSentencesDueToEnd(probationPractitioner),
     paroleReportsDue: getParoleReportsDue(probationPractitioner),
   })
-  res.render('v3/officer-view')
+  res.render('v6/officer-view')
 })
 
 router.get('/officer-view/:id/officer-view-cases',({ params: { id } }, res) => {
   res.locals.probationPractitioner = getPractitioner(id, team)
-  res.render('v3/officer-view-cases')
+  res.render('v6/officer-view-cases')
 })
 
 router.post('/allocate-handler', function (req, res) {
   var allocated = req.session.data['selected']
   if (allocated == "yes") {
-    res.redirect('/v3/allocation-complete')
+    res.redirect('/v6/allocation-complete')
   }
   else {
     res.redirect('/allocation-error')
