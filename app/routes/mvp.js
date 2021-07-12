7e40fd2f5f459bede6e86c24fc479bbe7f5b9ee0
theDustRoom <paul.massey@digital.justice.gov.uk>
2021-07-12 17:45:39 +0100

    ✨ Allow updating of contracted hours
    Signed-off-by: theDustRoom <paul.massey@digital.justice.gov.uk>

--- app/routes/mvp.js
+++ app/routes/mvp.js
@@ -13,38 +13,67 @@ const {
 const team = generateTeam(12, 'N57BRP', 'N57A')
 
 router.use('*', (req, { locals }, next) => {
-  Object.assign(locals, { team, teamTotals: getTeamTotals(team), url: req.originalUrl, versionUrl: '/_mvp', moment: moment, longDateFormat: 'D MMMM YYYY', shortDateFormat: 'DD/MM/YYYY' })
+  Object.assign(locals, {
+    team,
+    teamTotals: getTeamTotals(team),
+    url: req.originalUrl,
+    versionUrl: '/_mvp',
+    moment: moment,
+    longDateFormat: 'D MMMM YYYY',
+    shortDateFormat: 'DD/MM/YYYY'
+  })
   next()
 })
 
-router.get('/officer-view/:id', ({ params: { id } }, res) => {
-  const probationPractitioner = getPractitioner(id, team)
-  Object.assign(res.locals, {
+router.get('/officer-view/:id', ({ params: { id }, session }, res) => {
+  const probationPractitioner = !session[id] ? getPractitioner(id, team) : session[id].probationPractitioner
+  const data = {
     probationPractitioner,
     id: id,
     casesDueToEnd: getCasesDueToEnd(probationPractitioner),
     sentencesDueToEnd: getSentencesDueToEnd(probationPractitioner),
     paroleReportsDue: getParoleReportsDue(probationPractitioner),
-  })
+  }
+  Object.assign(res.locals, data)
+  Object.assign(session[id] || {}, data)
   res.render('_mvp/officer-view')
 })
 
-router.get('/officer-view/:id/officer-view-cases', ({ params: { id } }, res) => {
-  res.locals.probationPractitioner = getPractitioner(id, team)
-  res.locals.id = id
+router.get('/officer-view/:id/officer-view-cases', ({ params: { id }, session }, res) => {
+  const probationPractitioner = !session[id] ? getPractitioner(id, team) : session[id].probationPractitioner
+  Object.assign(res.locals, {
+    probationPractitioner,
+    id: id
+  })
   res.render('_mvp/officer-view-cases')
 })
 
-router.get('/officer-view/:id/reductions', ({ params: { id } }, res) => {
-  res.locals.probationPractitioner = getPractitioner(id, team)
-  res.locals.id = id
+router.get('/officer-view/:id/reductions', ({ params: { id }, session }, res) => {
+  const probationPractitioner = !session[id] ? getPractitioner(id, team) : session[id].probationPractitioner
+  Object.assign(res.locals, {
+    probationPractitioner,
+    id: id
+  })
   res.render('_mvp/officer-view-reductions')
 })
 
-router.get('/officer-view/:id/contracted-hours', ({ params: { id } }, res) => {
-  res.locals.probationPractitioner = getPractitioner(id, team)
-  res.locals.id = id
+router.get('/officer-view/:id/contracted-hours', ({ params: { id }, session }, res) => {
+  const probationPractitioner = !session[id] ? getPractitioner(id, team) : session[id].probationPractitioner
+  Object.assign(res.locals, {
+    probationPractitioner,
+    id: id
+  })
   res.render('_mvp/officer-view-contracted')
+})
+
+router.post('/officer-view/:id/contracted-hours', ({ body, params: { id }, session }, res) => {
+  const probationPractitioner = !session[id] ? getPractitioner(id, team) : session[id].probationPractitioner
+  probationPractitioner.contractedHours = body.contractedHours
+  Object.assign(session[id] || {}, {
+    probationPractitioner,
+    id: id
+  })
+  res.redirect(`/_mvp/officer-view/${id}`)
 })
 
 router.post('/allocate-handler', function (req, res) {
